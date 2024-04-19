f4f034bd00cef816554574e3dc072b1a3a0ed105
alibh021 <31588149+alibh021@users.noreply.github.com>
2024-04-20 02:14:59 +0330

    check client ips from log file every 1 min

--- web/web.go
+++ web/web.go
@@ -260,8 +260,8 @@ func (s *Server) startTask() {
 		s.cron.AddJob("@every 10s", job.NewXrayTrafficJob())
 	}()
 
-	// check client ips from log file every 10 sec
-	s.cron.AddJob("@every 10s", job.NewCheckClientIpJob())
+	// check client ips from log file every 1 min
+	s.cron.AddJob("@every 1m", job.NewCheckClientIpJob())
 
 	// check client ips from log file every day
 	s.cron.AddJob("@daily", job.NewClearLogsJob())
